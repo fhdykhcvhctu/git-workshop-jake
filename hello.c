9d9cb66209b349969bc2528a652dae1971e09083
jakedco_ubuntu_vm <jake@jaked.co>
2020-10-10 12:50:49 -0400

    new branch commit and edited hello.c

--- hello.c
+++ hello.c
@@ -5,6 +5,9 @@ int main(void)
 	// get this updated file from git pull
 	// modified to save
 
+	// THIS IS AN EDITED LINE FOR THE WORKSHOP BRANCH
+	printf("workshop branch\n");
+
 	// edited hello.c to not bleed into our terminal
 	printf("hello world!\n");
 
